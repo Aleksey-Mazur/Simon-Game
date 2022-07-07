55e574169f8288fac8a76004a2a6ccddd0bb7742
Aleksey <1aleksey.mazur@gmail.com>
2022-07-07 09:57:32 +0300

    comments have been added to game.js; favicon added

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ let userClickedPattern = [];
 let started = false;
 let level = 0;
 
+// Game will be started after any key have been pressed
 $(document).keypress(function () {
   if (!started) {
     $("#level-title").text("Level " + level);
@@ -13,6 +14,37 @@ $(document).keypress(function () {
   }
 });
 
+// Music that will be played after keypress or lose
+function playSound(name) {
+  const audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+// Animation when button was pressed
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(() => {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+// Pressing sequence
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
+  const randomNumber = Math.floor(Math.random() * 4);
+  const randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeOut(200)
+    .fadeIn(200);
+  playSound(randomChosenColour);
+}
+
+// Called after the user presses a button
 $(".btn").click(function () {
   let userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
@@ -23,6 +55,7 @@ $(".btn").click(function () {
   checkAnswer(userClickedPattern.length - 1);
 });
 
+// Check for matching clicks
 function checkAnswer(currentLevel) {
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
     console.log("success");
@@ -44,33 +77,7 @@ function checkAnswer(currentLevel) {
   }
 }
 
-function nextSequence() {
-  userClickedPattern = [];
-  level++;
-  $("#level-title").text("Level " + level);
-
-  const randomNumber = Math.floor(Math.random() * 4);
-  const randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-
-  $("#" + randomChosenColour)
-    .fadeOut(200)
-    .fadeIn(200);
-  playSound(randomChosenColour);
-}
-
-function playSound(name) {
-  const audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}
-
-function animatePress(currentColour) {
-  $("#" + currentColour).addClass("pressed");
-  setTimeout(() => {
-    $("#" + currentColour).removeClass("pressed");
-  }, 100);
-}
-
+// Start a new game
 function startOver() {
   level = 0;
   gamePattern = [];
